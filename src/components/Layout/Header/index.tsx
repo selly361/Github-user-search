ff521b8cb9d6642c75d1b31c2c6161397f5f74d5
seikou <seikou615@gmail.com>
2023-02-14 15:33:04 +0000

    Change: added index.ts file in order to export all components as default, to make importing components cleaner

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,14 +1,15 @@
 import { StyledHeader, ThemeSwitchContainer, ThemeText, Title } from './Header.styles'
 
+import type { IHeaderProps } from 'types/header'
 import React from 'react'
 
-function Header() {
+function Header({ toggleTheme, icon }: IHeaderProps) {
   return (
     <StyledHeader>
         <Title>DevFinder</Title>
-        <ThemeSwitchContainer>
+        <ThemeSwitchContainer onClick={toggleTheme}>
             <ThemeText></ThemeText>
-            
+            {icon}
         </ThemeSwitchContainer>
     </StyledHeader>
   )
