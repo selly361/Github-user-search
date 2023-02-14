c599f5bebac1cfd73439420905cc7ddc1ecc9e86
seikou <seikou615@gmail.com>
2023-02-14 15:36:20 +0000

    Change: added type for theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 
+type TTheme = "light" | "dark";
+
 export const useTheme = () => {
-  let defaultTheme =
-    localStorage.getItem("theme") ||
+  let defaultTheme: TTheme =
+    (localStorage.getItem("theme") as TTheme) ||
     (matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 
-  const [theme, setTheme] = useState(defaultTheme);
+  const [theme, setTheme] = useState<TTheme>(defaultTheme);
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
