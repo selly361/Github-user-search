47e4734963d61a041e595b857f23f8fbb66fb2b0
seikou <seikou615@gmail.com>
2023-02-14 15:31:16 +0000

    Change: added toggle theme function, query state, current theme styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 
 import themeStyles from 'global/theme'
+import { useTheme } from 'hooks'
 
 function App() {
 
+  const { theme, setTheme } = useTheme()
   
-  
+  const [query, setQuery] = useState("selly361")
 
+  const currentThemeStyles = themeStyles[theme]
+
+
+  const toggleTheme = () => setTheme(theme == "light" ? "dark" : "light")
+  
+  
+  
   
   return (
     null
